bed02eccf70069bafac5d2b0eb28f7ac921224ce
dario-ferrari <57733048+dario-ferrari@users.noreply.github.com>
2022-09-28 02:50:03 -0300

    Lista de usuarios y de clases inscriptas x alumno

---
+++ src/data/usuarios.js
@@ -0,0 +1,58 @@
+export default [
+        {
+            id: 0,
+            email: "enzoperez@gmail.com",
+            nombre: "Enzo Perez",
+            telefono: 49887375,
+            tipo: "ALUMNO"            
+        },
+        {
+            id: 1,
+            email: "mrodriguez@gmail.com",
+            nombre: "Maria Rodriguez",
+            telefono: 49029452,
+            tipo: "PROFESOR"            
+        },
+        {
+            id: 2,
+            email: "lucia-rossi@gmail.com",
+            nombre: "Lucia Rossi",
+            telefono: 1167402322,
+            tipo: "ALUMNO"           
+        },
+        {
+            id: 3,
+            email: "jgonzalez@gmail.com",
+            nombre: "Juan Gonzalez",
+            telefono: 1188934567,
+            tipo: "ALUMNO"            
+        },
+        {
+            id: 4,
+            email: "j-alvarez9@gmail.com",
+            nombre: "Julian Alvarez",
+            telefono: 47829595,
+            tipo: "PROFESOR"            
+        },
+        {
+            id: 5,
+            email: "agustinafernandez@gmail.com",
+            nombre: "Agustina Fernandez",
+            telefono: 1150221316,
+            tipo: "ALUMNO"            
+        },
+        {
+            id: 6,
+            email: "a",
+            nombre: "a",
+            telefono: 0,
+            tipo: "ALUMNO"            
+        },
+        {
+            id: 7,
+            email: "p",
+            nombre: "p",
+            telefono: 0,
+            tipo: "PROFESOR"            
+        }
+]
