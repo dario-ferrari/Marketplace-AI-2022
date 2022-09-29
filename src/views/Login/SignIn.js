ad7a7da4091fabf37b9487c5bd7fac006753a189
dario-ferrari <57733048+dario-ferrari@users.noreply.github.com>
2022-09-29 04:47:45 -0300

    Login arreglado

--- src/views/Login/SignIn.js
+++ src/views/Login/SignIn.js
@@ -14,7 +14,6 @@ import usuarios from "../../data/usuarios.js";
 const SignIn = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
 
   const handleLogIn = () => {
     {/**Método que se activa al clickear el botón de Login */}
@@ -28,7 +27,7 @@ const SignIn = () => {
         })
       );
       dispatch(loggIn());
-       
+      
       if (user.tipo === "PROFESOR") {
         navigate("/profesor");
       } else {
@@ -49,7 +48,7 @@ const SignIn = () => {
     <div className="w-screen h-screen flex justify-center items-center ">
       <div className="bg-white w-1/3 max-w-2xl h-3/5 max-h-2xl py-10 rounded-lg">
         <h1 className="uppercase text-6xl text-center font-light">Sign In</h1>
-        <form className="h-4/6 max-w-sm my-14 mx-auto flex flex-col justify-between">
+        <div className="h-4/6 max-w-sm my-14 mx-auto flex flex-col justify-between">
           <div className="h-2/5 flex flex-col justify-around">
             <InputForm type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
             <InputForm type="password" placeholder="Contraseña" />
@@ -63,7 +62,7 @@ const SignIn = () => {
               </a>
             </Link>
           </p>
-        </form>
+        </div>
       </div>
     </div>
   );
