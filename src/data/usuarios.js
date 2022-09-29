0e0508db650c6049c49d318bc7e62581ef448cf2
dario-ferrari <57733048+dario-ferrari@users.noreply.github.com>
2022-09-29 04:58:03 -0300

    Nuevo perfil

--- src/data/usuarios.js
+++ src/data/usuarios.js
@@ -2,65 +2,81 @@ export default [
         {
             id: 0,
             email: "enzoperez@gmail.com",
-            nombre: "Enzo Perez",
+            nombre: "Enzo",
+            apellido:"Perez",
             telefono: 49887375,
             tipo: "ALUMNO",
-            avatar: "https://images.ole.com.ar/2020/06/30/CSeyjc6ce_720x0__1.jpg"            
+            avatar: "https://www.cariverplate.com.ar/imagenes/jugadores/2022-08/1498-24-perez-imagenprincipal.png",
+            nacimiento: "22/2/1986"           
         },
         {
             id: 1,
             email: "mrodriguez@gmail.com",
-            nombre: "Maria Rodriguez",
+            nombre: "Maria",
+            apellido:"Rodriguez",
             telefono: 49029452,
             tipo: "PROFESOR",
-            avatar: "https://img.freepik.com/fotos-premium/joven-mujer-bonita-caucasica-blanco_1187-75943.jpg?w=2000"            
+            avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png",
+            nacimiento: "22/4/1995"           
         },
         {
             id: 2,
             email: "lucia-rossi@gmail.com",
-            nombre: "Lucia Rossi",
+            nombre: "Lucia",
+            apellido:"Rossi",
             telefono: 1167402322,
             tipo: "ALUMNO",
-            avatar: "https://img.freepik.com/foto-gratis/mujer-joven-rubia-confiada-brazos-cruzados-que-oponen-al-fondo-blanco_23-2148028933.jpg?w=2000"           
+            avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png",
+            nacimiento: "22/4/1995"           
         },
         {
             id: 3,
             email: "jgonzalez@gmail.com",
-            nombre: "Juan Gonzalez",
+            nombre: "Juan",
+            apellido:"Gonzalez",
             telefono: 1188934567,
             tipo: "ALUMNO",
-            avatar: "https://media.istockphoto.com/photos/portrait-of-young-smiling-caucasian-man-with-crossed-arms-wearing-picture-id1171169107?k=20&m=1171169107&s=612x612&w=0&h=VkbOnU3zWYCbh3ojzqWFhK7JObxQduGzr4bRxqbgkLU="            
+            avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png",
+            nacimiento: "22/4/1995"            
         },
         {
             id: 4,
             email: "j-alvarez9@gmail.com",
-            nombre: "Julian Alvarez",
+            nombre: "Julian",
+            apellido:"Alvarez",
             telefono: 47829595,
             tipo: "PROFESOR",
-            avatar:"https://cloudfront-us-east-1.images.arcpublishing.com/infobae/OSFKBBR4X5AGLCO4L4SDJYGHCE.jpg"            
+            avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png",
+            nacimiento: "22/4/1995"            
         },
         {
             id: 5,
             email: "agustinafernandez@gmail.com",
-            nombre: "Agustina Fernandez",
+            nombre: "Agustina",
+            apellido:"Fernandez",
             telefono: 1150221316,
             tipo: "ALUMNO",
-            avatar: "https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg?w=2000"            
+            avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png",
+            nacimiento: "22/4/1995"            
         },
         {
             id: 6,
             email: "a",
             nombre: "a",
+            apellido:"",
             telefono: 0,
             tipo: "ALUMNO",
-            avatar:"https://www.mundoprimaria.com/wp-content/uploads/2019/08/dibujos-letras-a-mayuscula-colorear.jpg"            
+            avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png",
+            nacimiento: "22/4/1995"            
         },
         {
             id: 7,
             email: "p",
             nombre: "p",
+            apellido:"",
             telefono: 0,
             tipo: "PROFESOR",
-            avatar: "https://i.pinimg.com/736x/8b/c8/87/8bc887b9623445dda21f99f91598e0b3.jpg"            
+            avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png",
+            nacimiento: "22/4/1995"            
         }
 ]
