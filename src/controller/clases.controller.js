bed1d8ee752f26dd86a5744b6283e7b064735b98
JuanIgnacioUrru <94595304+JuanIgnacioUrru@users.noreply.github.com>
2022-11-19 00:23:53 -0300

    cambios del viernes 19/11

---
+++ src/controller/clases.controller.js
@@ -0,0 +1,301 @@
+import urlWebServices from './webServices.js';
+
+
+
+export const guardarImgUser = async function(message)
+{
+    //url webservices
+    let url = urlWebServices.guardarImgUser;
+    //console.log("url",url);
+    //console.log("token",WebToken.webToken);
+    const formData = new URLSearchParams();
+    formData.append('email', message.email);
+    formData.append('nombreImagen',message.imagen);
+    
+    try
+    {
+        let response = await fetch(url,{
+            method: 'POST', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+                'x-access-token': localStorage.getItem('x'),
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            body:formData
+        });
+        if (response.status===201)
+        {
+            return true;
+        }
+        else
+        {
+           return false; 
+        }
+    }
+    catch(error)
+    {
+        console.log("error",error);
+        return false;
+    };
+}
+
+export const uploadFileImg= async function(files,nombres)
+{
+     //url webservices
+     let url = urlWebServices.uploadFileImg;
+  console.log("url",url)
+    console.log('files', files)
+    console.log('nombres',nombres)
+    const formData = new FormData();
+    //agrego archivos para subir
+    for (let i = 0; i < files.length; i++)
+    {
+        formData.append('files', files[i], nombres[i])
+    }
+   
+    try
+    {
+        let response = await fetch(url,{
+            method: 'POST', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/form-data',
+                'x-access-token': localStorage.getItem('x'),
+                'Origin':'http://localhost:3000',
+                //'Content-Type': 'application/form-data'
+            },
+            body:formData
+        });
+    
+        let archivos = await response.json()
+        console.log('respuestaUpload', archivos);
+        return archivos;
+    } catch (err) {
+        alert('Error uploading the files')
+        console.log('Error uploading the files', err)
+    }
+}
+export const listadoClases = async function()
+{
+    //url webservices
+    let url = urlWebServices.obtenerClases;
+    //console.log("url",url);
+    //console.log("token",WebToken.webToken);
+    //const formData = new URLSearchParams();
+    //formData.append('email', localStorage.getItem('email'));
+    
+    try
+    {
+        let response = await fetch(url,{
+            method: 'GET', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+                //'x-access-token': localStorage.getItem('x'),
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            //body:formData
+        });
+        if (response.status===200)
+        {
+            let data = await response.json();
+            console.log("Clases",data);
+            let listaClases = data.data.docs;
+            return listaClases;
+        }
+        else
+        {
+            let vacio=[];
+            console.log("No hay clases")
+            return (vacio);
+            
+        }
+    }
+    catch(error)
+    {
+        console.log("error",error);
+    };
+}
+
+export const buscarClasePorNombre = async function(titulo)
+{
+    //url webservices
+    let url = urlWebServices.obtenerClasesPorNombre;
+    //console.log("url",url);
+    //console.log("token",WebToken.webToken);
+    const formData = new URLSearchParams();
+    formData.append('titulo', titulo);
+
+    console.log("titulo", titulo )
+    
+    try
+    {
+        let response = await fetch(url,{
+            method: 'POST', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+                //'x-access-token': localStorage.getItem('x'),
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            body:formData
+        });
+        if (response.status===200)
+        {
+            let data = await response.json();
+            console.log("Clase",data);
+            let listaClases = data.data.docs;
+            return listaClases;
+        }
+        else
+        {
+            let vacio=[];
+            console.log("No hay clases")
+            return (vacio);
+            
+        }
+    }
+    catch(error)
+    {
+        console.log("error",error);
+    };
+}
+
+export const buscarClasePorId = async function(id)
+{//url webservices
+    let url = urlWebServices.obtenerClasesPorId;
+    //armo json con datos
+    const formData = new URLSearchParams();
+    formData.append('id', id);
+    //console.log("dato",formData);
+    //console.log("url",url);
+    try{
+        let response = await fetch(url,{
+            method: 'POST', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+               // 'x-access-token': WebToken.webToken,
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            body: formData,
+            
+        });
+        
+        let rdo = response.status;
+        console.log("response",response);
+        let data = await response.json();
+        console.log("jsonresponse",data);
+            switch(rdo){
+                case 200:
+                {
+                    return ({rdo:0, doctor:data.doctor});//correcto
+                }
+                default:
+                {
+                    //otro error
+                    return ({rdo:1,mensaje:data.message});                
+                }
+            }
+    }
+    catch(error){
+        console.log("error",error);
+    };
+
+}
+
+export const crearClaseNueva = async function(clase)
+{
+    //url webservices
+    let url = urlWebServices.crearClase;
+    //console.log("url",url);
+    //console.log("token",WebToken.webToken);
+    const formData = new URLSearchParams();
+    formData.append('titulo', clase.titulo)
+    formData.append('imagen', clase.imagen)
+    formData.append('descripcion', clase.descripcion)
+    formData.append('frecuencia', clase.frecuencia)
+    formData.append('duracion', clase.duracion)
+    formData.append('fechaLimite', clase.fechaLimite)
+    formData.append('precio', clase.precio)
+    formData.append('tipo', clase.tipo)
+    formData.append('rating', clase.rating)
+    formData.append('Usuarios_id', clase.Usuarios_id)
+    formData.append('disponibilidad', clase.disponibilidad)
+    formData.append('comentarios', clase.comentarios)
+
+    try
+    {
+        let response = await fetch(url,{
+            method: 'POST', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+                //'x-access-token': localStorage.getItem('x'),
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            body:formData
+        });
+        let rdo = response.status;
+        let dataBack = await response.json();
+        switch(rdo){
+            case 201:
+            {                    
+                return ({rdo:0,dataBack});//correcto
+            }
+            default:
+            {
+                //otro error
+                return ({rdo:1,mensaje:dataBack.message});                
+            }
+        }
+    }
+    catch(error)
+    {
+        console.log("error",error);
+        return false;
+    };
+}
+
+export const eliminadorClases = async function(id)
+{
+    console.log("esto recibe el front controller")
+    //url webservices
+    let url = urlWebServices.borrarClase;
+    //console.log("url",url);
+    //console.log("token",WebToken.webToken);
+    const formData = new URLSearchParams();
+    formData.append('_id', id)
+    try
+    {
+        let response = await fetch(url,{
+            method: 'DELETE', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+                //'x-access-token': localStorage.getItem('x'),
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            body:formData
+        });
+        let rdo = response.status;
+        console.log(response)
+        switch(rdo){
+            case 200:
+            {                    
+                return ({rdo:0,mensaje:"Se elimino"});//correcto
+            }
+            default:
+            {
+                //otro error
+                return ({rdo:1,mensaje:"No se elimino"});                
+            }
+        }
+    }
+    catch(error)
+    {
+        console.log("error",error);
+        return false;
+    };
+}
---
+++ src/controller/webServices.js
@@ -0,0 +1,26 @@
+const urlApi = "http://localhost:4000/";
+//const urlApi = "https://api-viernes.herokuapp.com/";
+console.log("url",urlApi);
+
+const urlWebServices = {
+    login:urlApi +"usuarios/login",
+    registration:urlApi +"usuarios/registration",
+    obtenerUsuarios : urlApi + "usuarios/all",
+    actualizarUsuario : urlApi + "usuarios/update",
+    borrarUsuario : urlApi + "usuarios/delete/:id",
+    //guardarImgUser: urlApi + "users/guardarImgUser",
+    //getImgUser: urlApi + "users/imgUserByMail",
+    //uploadFileImg: urlApi + "users/uploadImg",
+
+
+    crearClase : urlApi +"clases/create",
+    obtenerClases : urlApi +"clases/all",
+    obtenerClasesPorNombre: urlApi +"clases/:titulo",
+    obtenerClasesPorId : urlApi + "clases/id",
+    actualizarClase : urlApi +"clases/edit",
+    borrarClase: urlApi + "clases/delete"
+
+
+}
+
+export default urlWebServices;
