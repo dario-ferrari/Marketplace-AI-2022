bfd0d3cbe83a7324ec78eb5b825098b3b4401ba8
JuanIgnacioUrru <94595304+JuanIgnacioUrru@users.noreply.github.com>
2022-12-01 20:39:44 -0300

    modal compras

--- src/components/ClaseDetallada.js
+++ src/components/ClaseDetallada.js
@@ -3,7 +3,7 @@ import { Image } from "mui-image";
 import { Box, Divider, Grid, Typography,Button ,List,
   ListItem,
   ListItemText,
-  ListItemAvatar,Avatar,TextField } from "@mui/material";
+  ListItemAvatar,Avatar,TextField,FormGroup, Paper } from "@mui/material";
 import { buscarClasePorId } from '../controller/clases.controller';
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
@@ -18,15 +18,22 @@ import Swal from "sweetalert2";
 import { buscarUsuarioPorId } from "../controller/usuarios.controller";
 import Comentarios from "./componentsChiquitos/comentarios"
 import { UserContext } from '../Contexts/UserContext';
-import { crearContratacionNueva } from "../controller/contratacion.controller";import Modal from '@mui/material/Modal';
+import { crearContratacionNueva } from "../controller/contratacion.controller";
+import Modal from '@mui/material/Modal';
 
 export default function ClaseDetallada(props) {
-  
+
+  const paperStyle = {
+    padding: "30px 20px",
+    width: 500,
+    margin: "0px auto"
+  };
+
+
   const user = props.user
   const clase = props.clase
   const profe = props.profe
 
-  const [open, setOpen] = React.useState(false);
 
   console.log(profe,clase,user)
   const [obtenida,setObtenida] = React.useState(false)
@@ -37,12 +44,18 @@ export default function ClaseDetallada(props) {
     alumno : user._id,  
     profesor: profe._id,
     mensaje: "",
-    telefono: '',
-    email:'',
-    horarioRef:0,
+    telefono: "",
+    email:"",
+    horarioRef: null,
     isValorada: false
 
   })
+
+  let disabled = nuevaContratacion.mensaje === "" || nuevaContratacion.telefono === "" || nuevaContratacion.email === "" || nuevaContratacion.horarioRef === null ? true : false
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   React.useState(()=>{
       user.contrataciones.forEach(element => {
@@ -54,160 +67,281 @@ export default function ClaseDetallada(props) {
       
     }
   ,[])
+
+
+  const handleChange = (event) => {
+    setContratacion({
+      ...nuevaContratacion,
+      [event.target.name]: event.target.value
+    })
+  };
+
+  const handleCancel = ()=>{
+    setContratacion({
+      estado:'PENDIENTE',
+      clase: clase._id,
+      alumno : user._id,  
+      profesor: profe._id,
+      mensaje: "",
+      telefono: "",
+      email:"",
+      horarioRef: null,
+      isValorada: false
+    })
+    handleClose()
+  }
+
   
 
-  const comprarClase = ()=>{
-    const {values : formulario } = Swal.fire({
-    title: 'Multiple inputs',
-    html:
-      '<input id="swal-input1" class="swal2-input" placeholder="Telefono">' +
-      '<input id="swal-input2" class="swal2-input" placeholder="Email">' +
-      '<input type="number"id="swal-input3" class="swal2-input" placeholder="Horario de Contacto (24hs)">' +
-      '<textarea id="swal-textarea" class="swal2-input" placeholder="Mensaje">',
-    focusConfirm: false,
-    preConfirm: () => {
-        setContratacion({...nuevaContratacion, 
-          telefono : document.getElementById('swal-input1').value})
-        setContratacion({...nuevaContratacion, 
-          email : document.getElementById('swal-input2').value})
-        setContratacion({...nuevaContratacion, 
-          horarioRef : document.getElementById('swal-input3').value})
-        setContratacion({...nuevaContratacion, 
-          mensaje : document.getElementById('swal-textarea').value})
-        console.log(nuevaContratacion)
-    }
-  })
-}
+  const handleSubmit = ()=>{
+    /* const updateUser = async function () {
+      const respuesta = await actualizarUser(nuevaData,currentUser)
+      console.log(
+        "Console log de respuesta de back ",
+        JSON.stringify(respuesta)
+      );
+      if (respuesta.rdo === 1) {
+        alert("Ocurrio un error al guardar");
+      } else { */
+        Swal.fire({
+          icon: 'success',
+          title: 'Your work has been saved',
+          showConfirmButton: false,
+        })/* 
+      }
+    }; */
+  
+    handleClose()
+  }
 
 
-<Modal
-  open={open}
-  onClose={handleClose}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-  <Box sx={style}>
-    <Typography id="modal-modal-title" variant="h6" component="h2">
-      Text in a modal
-    </Typography>
-    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-    </Typography>
-  </Box>
-</Modal>
+
 
 
 
   return (
     <>
-      <Box component="main" sx={{ flex: 1, bgcolor: "#eaeff1"}}>
-      {/**la imagen de cover estaria bueno que se achicara un poco al bajar, investigar */}
-      <Image
-        src={clase.imagen}
-        fit="cover"
-        height="150px"
-        style={{
-          objectPosition: "50% 25%",
-          minHeight:"150px",
-          top: 0,
-          height: "150px",
-          width: "100%",
-          zIndex: "1"
-        }}
-      />
-      {/**titulo de la clase  */}
-      <Grid container>
-        <Grid item xs={10}>
-            <Typography variant="h1" py={3} px={5}>
-                {clase.titulo}
+      <Modal style={paperStyle} open={open}
+        onClose={handleClose}>
+        <Paper style={paperStyle}>
+        <Grid>
+          <Grid align="center">
+            <Typography variant="h5">Comprar Clase</Typography>
+            <Typography variant="body1">
+              Por favor rellena el formulario de contacto
             </Typography>
-        <Divider></Divider>
+          </Grid>
+          <FormGroup>
+            <TextField
+            margin="dense"
+              label="Email"
+              fullWidth
+              name="email"
+              placeholder="Enter email"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+            margin="dense"
+              label="Numero de telefono"
+              fullWidth
+              name="telefono"
+              placeholder="Pon Tu numero de contacto"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+            margin="dense"
+              label="Horario de contacto"
+              name="horarioRef"
+              type="number"
+              fullWidth
+              placeholder="Tu horario de contacto (24hs)"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+              label="Mensaje"
+              fullWidth
+              name="mensaje"
+              placeholder="Dejale un mensaje al profesor"
+              margin="dense"
+              multiline
+              rows="5"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <Button
+              sx={{ margin: "1vh" }}
+              color="success"
+              variant="contained"
+              disabled={disabled}
+              onClick={handleSubmit}
+            >
+              Guardar
+            </Button>
+            <Button  sx={{ margin: "1vh" }} color="error" variant="contained" onClick={handleCancel}>
+              Cancelar
+            </Button>
+          </FormGroup>
         </Grid>
-        <Grid item xs={1} container justifyContent={"space-between"}
-        alignContent={"center"} sx={{paddingRight:"1vh", margin:"0"}}>
-          <Grid item alignSelf={"center"}>
-            {(obtenida)?(
-              <Button size="large" variant="contained" color="primary" startIcon={<StarIcon/>}>Valorar</Button>
-            ):(
-              <Button onClick={comprarClase} size="large" variant="contained" color="success" startIcon={<ShoppingCartIcon/>}  >Comprar</Button> )
-            }
-            </Grid>
-        </Grid>
-        
-        {/* seccion donde se describe el curso */}
-        <Grid item xs={8}>
-          <Typography variant="body1" py={5} px={5}>
-            {clase.descripcion}
-          </Typography>
-        </Grid>
-
-        {/* seccion donde se describe al maetro */
-        (profe === null) ? (<Typography>Cargando</Typography>
-        ):(
-        <ContenedorProfesor nombre={profe.nombre} linkFoto={profe.avatar}
-        cantClases={profe.clasesPublicadas.length} experiencia={profe.experiencia} titulo ={profe.titulo}
+        </Paper>
+      </Modal>
+      <Box component="main" sx={{ flex: 1, bgcolor: "#eaeff1" }}>
+        {/**la imagen de cover estaria bueno que se achicara un poco al bajar, investigar */}
+        <Image
+          src={clase.imagen}
+          fit="cover"
+          height="150px"
+          style={{
+            objectPosition: "50% 25%",
+            minHeight: "150px",
+            top: 0,
+            height: "150px",
+            width: "100%",
+            zIndex: "1",
+          }}
         />
-        )
-        }
-        {/*caracteristicas de la clase la barrita con duracion,frcuencia,etc,etc */}
-        <Grid
-          container
-          height={"10em"}
-          marginY={"5em"}
-          direction="row"
-          alignItems="center"
-          justifyContent="space-around"
-          sx={{ bgcolor: "#e2e3e3",boxShadow: " inset 0 0px 8px 10px rgba(0, 0, 0, 0.15)"}}
-        >
-          <ItemBarrita icono={<AccessTimeIcon sx={{ fontSize: "3.5em" }} />} descripcion={clase.duracion + ' hs'} />
+        {/**titulo de la clase  */}
+        <Grid container>
+          <Grid item xs={10}>
+            <Typography variant="h1" py={3} px={5}>
+              {clase.titulo}
+            </Typography>
+            <Divider></Divider>
+          </Grid>
+          <Grid
+            item
+            xs={1}
+            container
+            justifyContent={"space-between"}
+            alignContent={"center"}
+            sx={{ paddingRight: "1vh", margin: "0" }}
+          >
+            <Grid item alignSelf={"center"}>
+              {obtenida ? (
+                <Button
+                  size="large"
+                  variant="contained"
+                  color="primary"
+                  startIcon={<StarIcon />}
+                >
+                  Valorar
+                </Button>
+              ) : (
+                <Button
+                  onClick={handleOpen}
+                  size="large"
+                  variant="contained"
+                  color="success"
+                  startIcon={<ShoppingCartIcon />}
+                >
+                  Comprar
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+          {/* seccion donde se describe el curso */}
+          <Grid item xs={8}>
+            <Typography variant="body1" py={5} px={5}>
+              {clase.descripcion}
+            </Typography>
+          </Grid>
 
-          <Divider orientation="vertical" variant="middle" flexItem ></Divider>
+          {
+            /* seccion donde se describe al maetro */
+            profe === null ? (
+              <Typography>Cargando</Typography>
+            ) : (
+              <ContenedorProfesor
+                nombre={profe.nombre}
+                linkFoto={profe.avatar}
+                cantClases={profe.clasesPublicadas.length}
+                experiencia={profe.experiencia}
+                titulo={profe.titulo}
+              />
+            )
+          }
+          {/*caracteristicas de la clase la barrita con duracion,frcuencia,etc,etc */}
+          <Grid
+            container
+            height={"10em"}
+            marginY={"5em"}
+            direction="row"
+            alignItems="center"
+            justifyContent="space-around"
+            sx={{
+              bgcolor: "#e2e3e3",
+              boxShadow: " inset 0 0px 8px 10px rgba(0, 0, 0, 0.15)",
+            }}
+          >
+            <ItemBarrita
+              icono={<AccessTimeIcon sx={{ fontSize: "3.5em" }} />}
+              descripcion={clase.duracion + " hs"}
+            />
 
-          <ItemBarrita icono={<CalendarMonthIcon sx={{ fontSize: "3.5em" }} />} descripcion={clase.frecuencia} />
+            <Divider orientation="vertical" variant="middle" flexItem></Divider>
 
-          <Divider orientation="vertical" variant="middle" flexItem></Divider>
+            <ItemBarrita
+              icono={<CalendarMonthIcon sx={{ fontSize: "3.5em" }} />}
+              descripcion={clase.frecuencia}
+            />
 
-          <ItemBarrita icono={<PaidIcon sx={{ fontSize: "3.5em" }} />} descripcion= {clase.precio + ' USD'} />
+            <Divider orientation="vertical" variant="middle" flexItem></Divider>
 
-          <Divider orientation="vertical" variant="middle" flexItem ></Divider>
-          
-          <ItemBarrita icono={<PersonIcon sx={{ fontSize: "3.5em" }} />} descripcion={clase.tipo} />
+            <ItemBarrita
+              icono={<PaidIcon sx={{ fontSize: "3.5em" }} />}
+              descripcion={clase.precio + " USD"}
+            />
 
+            <Divider orientation="vertical" variant="middle" flexItem></Divider>
+
+            <ItemBarrita
+              icono={<PersonIcon sx={{ fontSize: "3.5em" }} />}
+              descripcion={clase.tipo}
+            />
+          </Grid>
         </Grid>
-      </Grid>
 
-{/**donde van a ir los comentarios */}
-    <Typography variant="h3" paddingX={'1em'}>
-      Comentarios
-    </Typography>
-    <Comentarios comentarios={clase.comentarios}></Comentarios>
-  <Grid container paddingX={"4em"} paddingY={"3em"}>
-      <List sx={{ width: "100%" }}>
-        <React.Fragment></React.Fragment>
+        {/**donde van a ir los comentarios */}
+        <Typography variant="h3" paddingX={"1em"}>
+          Comentarios
+        </Typography>
+        <Comentarios comentarios={clase.comentarios}></Comentarios>
+        <Grid container paddingX={"4em"} paddingY={"3em"}>
+          <List sx={{ width: "100%" }}>
+            <React.Fragment></React.Fragment>
             <ListItem alignItems="flex-start">
-                <ListItemAvatar sx={{ paddingRight: "1ex" }}>
-                    <Avatar src={user.avatar}
-                        sx={{
-                        width: 70,
-                        height: 70,
-                        boxShadow: "0 8px 8px 0 rgba(0, 0, 0, 0.15)",
-                        }}
-                    ></Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                    primary={
-                      <TextField fullWidth id="standard-basic" label="Escribe tu comentario" variant="standard" sx={{ margin:"1ex"}}></TextField>
-                            }
-                    secondary={
-                    <React.Fragment>
-                            <Button variant="outlined">Comentar</Button>
-                    </React.Fragment>
-                            }
-                />
-          </ListItem>
-        </List>
-    </Grid>
-    </Box>   
-  </>
-  )
+              <ListItemAvatar sx={{ paddingRight: "1ex" }}>
+                <Avatar
+                  src={user.avatar}
+                  sx={{
+                    width: 70,
+                    height: 70,
+                    boxShadow: "0 8px 8px 0 rgba(0, 0, 0, 0.15)",
+                  }}
+                ></Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={
+                  <TextField
+                    fullWidth
+                    id="standard-basic"
+                    label="Escribe tu comentario"
+                    variant="standard"
+                    sx={{ margin: "1ex" }}
+                  ></TextField>
+                }
+                secondary={
+                  <React.Fragment>
+                    <Button variant="outlined">Comentar</Button>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          </List>
+        </Grid>
+      </Box>
+    </>
+  );
 }
